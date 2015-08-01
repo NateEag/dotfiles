fbc25959f42e56c4aa6412419de5c10f24de9095
Nate Eagleson <nate@nateeag.com>
2015-08-01 15:27:40 -0400

    Make check-site-links.sh run at most once per day

--- bin/check-site-links.sh
+++ bin/check-site-links.sh
@@ -2,7 +2,24 @@
 
 # Simple wrapper around linkchecker to check my personal websites for dead links.
 #
-# Meant to be run regularly via cron.
+# Meant to be run on successful network connection, so that after hopping on a
+# network for the first time in a day, I just get notified if the sites have
+# issues.
+
+DATESTAMP_PATH=~/.check-site-links-last-run
+
+# Format dates like numbers, so we can compare them as integers.
+NOW=$(date +'%Y%m%d')
+
+LAST_RUN=$(cat "$DATESTAMP_PATH")
+# If the datestamp can't be read, assume we've never run.
+LAST_RUN=${LAST_RUN:-0}
+
+if [ $NOW -le $LAST_RUN ]; then
+    echo "Already checked today."
+    exit
+fi
+
 
 SITES=("http://www.nateeag.com" "http://howicode.nateeag.com")
 
@@ -14,4 +31,6 @@ for SITE in ${SITES[*]}; do
         echo "This shouldn't make a difference..."
         growlnotify -s -m "$SITE has broken links."
     fi
+
+    echo "$NOW" > "$DATESTAMP_PATH"
 done
