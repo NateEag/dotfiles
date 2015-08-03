a17d61a661f34345b071adfec45f2f1cd447ec15
Nate Eagleson <nate@nateeag.com>
2015-08-02 20:10:17 -0400

    Various tweaks to check-site-links.sh
    It seems to be working happily via crankd now, which should be
    auto-started by launchd.

--- bin/check-site-links.sh
+++ bin/check-site-links.sh
@@ -5,6 +5,11 @@
 # Meant to be run on successful network connection, so that after hopping on a
 # network for the first time in a day, I just get notified if the sites have
 # issues.
+#
+# On OS X, crankd (part of https://github.com/nigelkersten/pymacadmin) is a decent
+# tool for running scripts on network status changes. I set it up as described
+# in this blog post:
+# http://nokyotsu.com/qscripts/2011/03/run-script-in-os-x-on-network.html
 
 # Constants.
 SITES=("http://www.nateeag.com" "http://howicode.nateeag.com")
@@ -23,7 +28,10 @@ if [ $NOW -le $LAST_RUN ]; then
     exit
 fi
 
-ping -c 1 ${SITES[0]:7}
+# Load my personal PATH and similar settings, since launchd doesn't include them.
+. /Users/nate/.bashrc
+
+/sbin/ping -c 1 ${SITES[0]:7}
 if [ $? -ne 0 ]; then
     echo "Cannot reach site; assuming no net connection."
     exit
@@ -33,16 +41,16 @@ SITES_OKAY=0
 for SITE in ${SITES[*]}; do
     linkchecker --check-extern "$SITE" 2> /dev/null
 
-    # FIXME Without this, growlnotify fails silently. Race condition?
-    echo "Checking $SITE..."
-
-    if [ "$?" != "0" ]; then
-        SITES_OKAY=1
+    if [ $? -ne 0 ]; then
+        # FIXME Without this echo, growlnotify fails silently. Race condition?
+        echo "Checked $SITE"
         growlnotify -s -m "$SITE has broken links."
-    fi
 
-    echo "$NOW" > "$DATESTAMP_PATH"
+        SITES_OKAY=1
+    fi
 done
+
+echo "$NOW" > "$DATESTAMP_PATH"
 
 if [ $SITES_OKAY -eq 0 ]; then
     # Explicitly telling us the site looks okay mitigates the risk of silent
