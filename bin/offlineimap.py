650e6174820b65fc70d90cf7a7fada7bad34f618
Nate Eagleson <nate@nateeag.com>
2015-10-17 07:39:22 -0400

    Use ~/.authinfo.gpg for offlineimap password
    It's more portable than the OS X-specific Keychain approach I had been
    using, and I was already using .authinfo.gpg for sending mail from Emacs.

--- bin/offlineimap.py
+++ bin/offlineimap.py
@@ -3,7 +3,20 @@
 # Functions for use with offlineimap.
 
 import re
+import os
 import subprocess
+
+
+def get_authinfo_password(machine, login):
+    "Return decrypted password from .authinfo.gpg."
+
+    s = 'machine %s login %s password "([^"]*)"' % (machine, login)
+    pattern = re.compile(s)
+
+    authinfo = os.popen("gpg -q -d ~/.authinfo.gpg").read()
+    match = pattern.search(authinfo)
+
+    return match.group(1)
 
 
 def get_keychain_pass(account=None, server=None):
