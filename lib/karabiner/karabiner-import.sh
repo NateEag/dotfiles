da6c49ce2cf6c43208d622e47e166eee04b571c7
Nate Eagleson <nate@nateeag.com>
2016-10-28 16:42:38 -0400

    Update Karabiner prefs to only apply to internalkeyboard
    and store the actual settings in the repo, thanks to the import/export
    script I didn't know existed until today.

---
+++ lib/karabiner/karabiner-import.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set private.F19ToshiftL 1
+/bin/echo -n .
+$cli set private.return2shiftR_return 1
+/bin/echo -n .
+$cli set private.shiftL2controlL 1
+/bin/echo -n .
+$cli set private.shiftR2controlR 1
+/bin/echo -n .
+$cli set repeat.initial_wait 416
+/bin/echo -n .
+$cli set repeat.wait 33
+/bin/echo -n .
+/bin/echo
