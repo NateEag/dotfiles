a3ff59c1ae514e8d517e38fae5b18b94a28071c9
Nate Eagleson <nate@nateeag.com>
2017-11-15 14:49:43 -0500

    Tweak the 'install python commands' logic
    mostly to note that I didn't automate the latest WIP.

--- bin/install-python-commands.sh
+++ bin/install-python-commands.sh
@@ -5,8 +5,6 @@
 # Assumes [pipsi](https://github.com/mitsuhiko/pipsi) and virtualenv are
 # installed (not via pyenv-virtualenv, because pipsi doesn't know about S).
 
-set -e
-
 if ! command -v pipsi; then
     curl https://raw.githubusercontent.com/mitsuhiko/pipsi/master/get-pipsi.py \
         | python
@@ -19,3 +17,9 @@ package_list="$project_dir/lib/python-commands.txt"
 while read -r package_name; do
     pipsi install "$package_name"
 done <"$package_list"
+
+echo "Remember to manually install chrome-pass with python3, then to run"
+echo
+echo "    nativePass install"
+echo
+echo "...or figure out how to automate the installation with Pipsi."
