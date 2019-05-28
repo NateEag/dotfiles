484ff4262b5f8dc486753d702178b58325115a47
Nate Eagleson <nate@nateeag.com>
2019-05-28 08:31:41 -0400

    Move window movement Ctrl+Shift+O binding from Slate to Hammerspoon
    Gotta fix that darned animation, though. I don't want it.

--- src/.hammerspoon/init.lua
+++ src/.hammerspoon/init.lua
@@ -71,3 +71,26 @@ hs.hotkey.bind(hyper_keys, "h", function()
     -- because who wants to wait for an animation?
     win:setFrame(win_frame)
 end)
+
+
+-- Move window to next monitor. Yanked from here:
+--
+-- https://www.reddit.com/r/hammerspoon/comments/aexm45/request_move_between_multiple_monitors/
+hs.hotkey.bind(hyper_keys, "O", function()
+    -- Get the focused window, its window frame dimensions, its screen frame
+    -- dimensions, and the next screen's frame dimensions.
+    local focusedWindow = hs.window.focusedWindow()
+    local focusedScreenFrame = focusedWindow:screen():frame()
+    local nextScreenFrame = focusedWindow:screen():next():frame()
+    local windowFrame = focusedWindow:frame()
+
+    -- Calculate the coordinates of the window frame in the next screen and
+    -- retain aspect ratio
+    windowFrame.x = ((((windowFrame.x - focusedScreenFrame.x) / focusedScreenFrame.w) * nextScreenFrame.w) + nextScreenFrame.x)
+    windowFrame.y = ((((windowFrame.y - focusedScreenFrame.y) / focusedScreenFrame.h) * nextScreenFrame.h) + nextScreenFrame.y)
+    windowFrame.h = ((windowFrame.h / focusedScreenFrame.h) * nextScreenFrame.h)
+    windowFrame.w = ((windowFrame.w / focusedScreenFrame.w) * nextScreenFrame.w)
+
+    -- Set the focused window's new frame dimensions
+    focusedWindow:setFrame(windowFrame)
+end)
