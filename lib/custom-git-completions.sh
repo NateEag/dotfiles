1dbde126a4496b5b52a8d4bbfd3b8a7e82362c8e
Nate Eagleson <nate@nateeag.com>
2019-10-10 10:48:46 -0400

    Add git-rename-branch script
    I had need to do it, including a remote branch.
    
    That was such an annoyingly kludgey operation I decided to automate it,
    so I don't fall down the path of writing my own VCS out of sheer nerd
    rage.
    
    If I go down that path I want it to be a well-reasoned decision, not a
    manifestation of my character flaws. :P
    
    (https://github.con/NateEag/next-vcs)

--- lib/custom-git-completions.sh
+++ lib/custom-git-completions.sh
@@ -3,7 +3,12 @@
 # Set up auto-completion for my custom git commands.
 
 _git_nuke_branch () {
-    __gitcomp_nl "$(__git_refs)"
+    __gitcomp_nl "$(__git_heads)"
 }
-
 export _git_nuke_branch
+
+
+_git_rename_branch () {
+    __gitcomp_nl "$(__git_heads)"
+}
+export _git_rename_branch
