634d09a0e13c9a28620429dad492967e84219086
Nate Eagleson <nate@nateeag.com>
2021-09-09 17:27:33 -0400

    Add several more packages to my Nix config

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -16,13 +16,39 @@
       myPackages = pkgs.buildEnv {
         name = "my-packages";
         paths = [
+          # The invocation I use to install my packages mean the nix tools get
+          # nuked if I don't explicitly include them.
           nix
+
+          # Make SSL certificates available? Apparently they get deleted if I
+          # don't explicitly install them, rendering nix-env unable to fetch
+          # files from HTTPS servers due to SSL cert verification failures.
+          # Therefore.
+          cacert
+
+          # Yay for diff coloration and prettiness!
           delta
+
+          # Password safes are handy.
+          pass
+
+          # I sometimes use pass as the source for Git credentials.
           pass-git-helper
+
+          # Tools I use for synchronizing and managing email.
           notmuch
           isync
+
+          # Send CLI notifications on OS X.
+          #
+          # TODO Only install this on OS X.
+          terminal-notifier
+
+          # Aspell isn't a perfect spellchecker, but it works reasonably well
+          # for me.
+          aspell
         ];
-        pathsToLink = [ "/share" "/bin" "/Applications" ];
+        pathsToLink = [ "/share" "/bin" "/etc" "/Applications" ];
       };
     };
 }
