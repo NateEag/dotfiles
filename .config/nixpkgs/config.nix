09f446ac467a2c43f0d0275ee51a7bd2bb923838
Nate Eagleson <nate@nateeag.com>
2022-01-06 10:57:14 -0500

    Add bash and bash completion packages to my Nix environment
    The goal is to get a working Bash 4 installation on my OS X desktop.
    
    I haven't quite gotten there yet, but the packages seem like a
    prequisite.

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -24,6 +24,18 @@
           # nuked if I don't explicitly include them.
           nix
 
+          # If you looked at my Emacs configuration, you'd probably expect me
+          # to be a zsh user with a heavily-customized environment, but I stick
+          # with bash because I wind up working on remote systems or ones I
+          # don't conrol a lot - I don't want to get used to idioms I won't
+          # have elsewhere.
+          #
+          # This is here mostly because I want to have a current version of
+          # bash on OS X.
+          bashInteractive
+          bash-completion
+          nix-bash-completions
+
           # In theory, nix could completely subsume direnv's purpose with
           # per-project Nix shells and configurations.
           #
